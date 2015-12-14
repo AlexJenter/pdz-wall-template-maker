554d44f75c4aefb0d1c5043e29a5dcedc2393912
AlexJenter <jenteralex@gmail.com>
2015-12-14 11:45:07 +0100

    commit for timon, clean up files names, nav etc pi pa po

--- js/main.js
+++ js/main.js
@@ -3,46 +3,50 @@ $(document).ready( function() {
     $('.content').load( "../svg/uebersicht.svg" );
 });
 
+
 // NAV
 function switchSVG( el, path ){
     el.click( function() {
-        $('.active').removeClass('active');
+        $('nav .active').removeClass('active');
         $(this).addClass('active');
         $('.content').load( path, function() {
             assignLabels();
         });
-
     });
-
 }
 
 switchSVG( $('#Home'),"../svg/uebersicht.svg");
-switchSVG( $('#sp'),  "../svg/schwerpunkt.svg");
-switchSVG( $('#ft'),  "../svg/thema.svg");
-switchSVG( $('#ma'),  "../svg/person.svg");
+switchSVG( $('#sp'),  "../svg/forschungsschwerpunkt.svg");
+switchSVG( $('#ft'),  "../svg/forschungsthema.svg");
+switchSVG( $('#ma'),  "../svg/mitarbeiter.svg");
 switchSVG( $('#st'),  "../svg/student.svg");
 
 
 // TEXT CHANGE
 function textChange( el, prompt ) {
     el.click( function() {
-        var oldContent =  $(this).text();
         var newContent = window.prompt( prompt );
-        if ( newContent ==="" ) { newContent = oldContent; }
+        if ( !newContent ) {
+            return;
+        }
         $(this).text( newContent );
-        if ( $(this).text() ==="" )  { $(this).text(oldContent) }
     });
 }
 
+
 function assignLabels(){
-    textChange( $('#vorname'), "Bitte Vorname eingeben...");
-    textChange( $('#name'),    "Bitte Name eingeben...");
-    textChange( $('#nachname'),"Bitte Nachname eingeben...");
-    textChange( $('#zimmer'),  "Bitte Zimmernummer eingeben...");
-    textChange( $('#tel'),     "Bitte Telefonnummer eingeben...");
-    textChange( $('#zeile-1'),     "Bitte erste Zeile eingeben...");
-    textChange( $('#zeile-2'),     "Bitte zweite Zeile eingeben...");
+    textChange( $('svg #vorname'),  "Bitte Vorname eingeben...");
+    textChange( $('svg #name'),     "Bitte Name eingeben...");
+    textChange( $('svg #nachname'), "Bitte Nachname eingeben...");
+    textChange( $('svg #zimmer'),   "Bitte Zimmernummer eingeben...");
+    textChange( $('svg #funktion'), "Bitte Funktion eingeben...( Semesterarbeit / MA Thesis )");
+    textChange( $('svg #tel'),      "Bitte Telefonnummer eingeben...");
+    textChange( $('svg #zeile-1'),  "Bitte erste Zeile eingeben...");
+    textChange( $('svg #zeile-2'),  "Bitte zweite Zeile eingeben...");
+    textChange( $('svg #zeile-3'),  "Bitte dritte Zeile eingeben...");
+    textChange( $('svg #zeile-4'),  "Bitte vierte Zeile eingeben...");
 }
+
 
 // DROP IMAGE
 function imageChange(){
@@ -51,34 +55,26 @@ function imageChange(){
         on: {
             load: function(e, file) {
                 var img = new Image();
-                $('image').attr('xlink:href', e.target.result);
+                $('svg image').attr('xlink:href', e.target.result);
                 img.src = e.target.result;
             }
         }
     });
 }
+
 imageChange();
 
 
 // COLORZ
-$('span').click( function() {
-    var c = $(this).css('background-color');
-    hexc(c);
-
-    function hexc(colorval) {
-        var parts = colorval.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-        delete(parts[0]);
-        for (var i = 1; i <= 3; ++i) {
-            parts[i] = parseInt(parts[i]).toString(16);
-            if (parts[i].length == 1) parts[i] = '0' + parts[i];
-        }
-        hexcol = '#' + parts.join('');
-    }
-    $('rect').css({fill: hexcol});
+$('.colors span').click( function() {
+    var fillColor = $(this).css('background-color');
+    $('svg #farbfläche').css({fill: fillColor});
 });
 
+
+/*
 // EXPORT PNG > PDF
-$('.fa-cloud-download').click( function () {
+$('.saveAsPdf').click( function () {
     svgAsPngUri( $("svg")[0], {scale: 4.166}, function(uri) {
         var img = uri;
         var doc = new jsPDF();
@@ -86,12 +82,26 @@ $('.fa-cloud-download').click( function () {
         doc.save('test.pdf');
     });
 });
+*/
 
-// EXPORT SVG
-// $('.fa-download').click( function() {
-//     var exportSVG =  $('svg').outerHTML();
-//     var blob = new Blob([ exportSVG ], {type: "image/svg+xml"});
-//     saveAs(blob, "export.svg");
-//     // demoSvgDocument();
-//     // console.log( blob );
+// EXPORT PNG > PDF
+// $('.saveAsPdf').click( function () {
+//
+//     var doc = new jsPDF();
+//     svgElementToPdf($("svg")[0],doc,{ x_offset: 25, y_offset: 50 });
+//
+//     doc.save('test.pdf');
+//
+//
+//
+//
+//     svgElementToPdf(0,0,0);
 // });
+$('.saveAsPdf').click( function () {
+    svgAsPngUri( $("svg")[0], {scale: 4.166}, function(uri) {
+        var img = uri;
+        var doc = new jsPDF();
+        doc.addImage(img, 'PNG', 30, 50, 150, 150);
+        doc.save('test.pdf');
+    });
+});
